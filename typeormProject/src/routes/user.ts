b7589dc313838f259048a563d45a4358e94fbd9b
Fabian Ramirez <ramirez1988fabian@gmail.com>
2021-01-08 15:56:17 -0500

    Establishing routes and getting controllers

--- typeormProject/src/routes/user.ts
+++ typeormProject/src/routes/user.ts
@@ -1,5 +1,13 @@
-import { Router } from 'express'
+import { Router } from 'express';
+import { getUser, getUsers, postUser, updateUser, deleteUser } from '../controllers/user'
+
 
 const router = Router();
+
+router.get('/users',getUsers);
+router.get('/users/:id', getUser);
+router.post('/users', postUser);
+router.put('/users/:id', updateUser);
+router.delete('/users/:id', deleteUser);
 
 export default router
