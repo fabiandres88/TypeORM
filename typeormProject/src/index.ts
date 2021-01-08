aeecf01acf469ebff7f5bbabf3b2c93427df7685
Fabian Ramirez <ramirez1988fabian@gmail.com>
2021-01-08 15:54:40 -0500

    Adding database connection

--- typeormProject/src/index.ts
+++ typeormProject/src/index.ts
@@ -1,14 +1,22 @@
+import 'reflect-metadata';
 import express = require('express');
 import morgan = require ('morgan');
-import userRoutes from './routes/user';   
+import userRoutes from './routes/user';
+import { createConnection } from 'typeorm' 
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.json());
 
-//routes
+//Routes
 app.use(userRoutes);
 
+//Databsae connection
+createConnection().then(async connection => { 
+   console.log("TypeORM with MongoDB"); 
+ }).catch(error => console.log(error));
+
+ //Server connection
 app.listen(3000);
 console.log('Server ready on port: ', 3000);
