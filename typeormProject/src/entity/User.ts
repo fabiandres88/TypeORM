062244eccbfb51b86743adc02d28467f27602954
Fabian Ramirez <ramirez1988fabian@gmail.com>
2021-01-09 18:55:45 -0500

    Adding properties validations with class-validator module

--- typeormProject/src/entity/User.ts
+++ typeormProject/src/entity/User.ts
@@ -1,17 +1,33 @@
-import {Entity, ObjectIdColumn, ObjectID, Column} from 'typeorm';
+import { IsEmail, IsInt, Length, Max, Min } from 'class-validator';
+import { Entity, ObjectIdColumn, ObjectID, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 
-@Entity( {name: 'users'} )
+@Entity({ name: 'users' })
 export class User {
 
     @ObjectIdColumn()
     id: ObjectID;
 
     @Column()
+    @Length(3, 50)
     firstName: string;
-
+    
     @Column()
+    @Length(3, 50)
     lastName: string;
 
     @Column()
+    @IsInt()
+    @Min(0)
+    @Max(99)
     age: number;
-}
+
+    @Column()
+    @IsEmail()
+    email: string;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+};
