91173a662b635e24db64781a9c8e965cf8220f5e
Fabian Ramirez <ramirez1988fabian@gmail.com>
2021-01-09 18:57:17 -0500

    Adding function validate in create and update user

--- typeormProject/src/controllers/user.ts
+++ typeormProject/src/controllers/user.ts
@@ -1,34 +1,75 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import { User } from '../entity/User';
+import { validate } from 'class-validator';
 
 export const getUsers = async (req: Request, res: Response): Promise<Response> =>{
-    const users = await getRepository(User).find();
-    return res.status(200).json(users);
+    try {
+        const users = await getRepository(User).find();
+        return res.status(200).json(users);
+    }
+    catch (error) {
+        console.log('Error: ', error.message);
+        return res.status(400).send();
+    };    
 };
 
 export const getUser = async (req: Request, res: Response): Promise<Response> =>{
-    const user = await getRepository(User).findOne(req.params.id);
-    return res.status(200).json(user);
+    try {
+        const user = await getRepository(User).findOne(req.params.id);
+        return res.status(200).json(user);
+    }
+    catch (error) {
+        console.log('Error: ', error.message);
+        return res.status(400).send();
+    };    
 };
 
 export const postUser = async (req: Request, res: Response): Promise<Response> =>{
-    const user = getRepository(User).create(req.body);
-    const response = await getRepository(User).save(user);
-    return res.status(200).json(response);
+    try{
+        const user = getRepository(User).create(req.body);
+        const validations = await validate(user);
+
+        if(validations.length === 0) {
+            const response = await getRepository(User).save(user);
+            return res.status(201).json(response);
+        }
+        res.status(400).json(validations);
+    }
+    catch (error) {
+        console.log('Error: ', error.message);
+        return res.status(400).send();
+    };    
 };
 
 export const updateUser = async (req: Request, res: Response): Promise<Response> =>{
-    const user = await getRepository(User).findOne(req.params.id);
+    try {
+        const user = await getRepository(User).findOne(req.params.id);
     if (user) {
         getRepository(User).merge(user, req.body);
-        const response = await getRepository(User).save(user);
-        return res.status(200).json(response);
+        const validations = await validate(user);
+
+        if (validations.length === 0) {
+            const response = await getRepository(User).save(user);
+            return res.status(200).json(response);    
+        }
+        res.status(400).json(validations);        
     }
     return res.status(404).json({message: "User does not exit."});    
+    }
+    catch (error) {
+        console.log('Error: ', error.message);
+        return res.status(400).send();
+    };    
 };
 
 export const deleteUser = async (req: Request, res: Response): Promise<Response> =>{
-    const result = getRepository(User).delete(req.params.id);
-    return res.status(200).json({message: "User deleted."});
+    try {
+        const result = getRepository(User).delete(req.params.id);
+        return res.status(200).json({message: "User deleted."});
+    }
+    catch (error) {
+        console.log('Error: ', error.message);
+        return res.status(400).send();
+    };    
 };
