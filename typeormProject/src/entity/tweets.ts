2b331e7d3bfb796f66096eb1fa6fef27064309dc
Fabian Ramirez <ramirez1988fabian@gmail.com>
2021-01-09 19:53:23 -0500

    Adding decorators to validate title and content fields in message model with custom error messages

--- typeormProject/src/entity/tweets.ts
+++ typeormProject/src/entity/tweets.ts
@@ -1,5 +1,5 @@
-import {Entity, ObjectIdColumn, ObjectID, Column, ManyToOne} from 'typeorm';
-import { User } from './User';
+import {Entity, ObjectIdColumn, ObjectID, Column} from 'typeorm';
+import {Length} from 'class-validator';
 
 @Entity( {name: 'tweets'} )
 export class Tweet {
@@ -8,11 +8,13 @@ export class Tweet {
     id: ObjectID;
 
     @Column()
+    @Length(2, 50, {message: `Title must be at least 2 but not longer than 50 characters.`})
     title: string;
 
     @Column()
+    @Length(2, 100, {message: `Title must be at least 2 but not longer than 100 characters.`})
     content: string;
 
     @Column()
     user: string    
-}
+};
