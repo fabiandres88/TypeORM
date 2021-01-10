a3c8c5efd7b4b08131819aaed2abb4c8988cd8f9
Fabian Ramirez <ramirez1988fabian@gmail.com>
2021-01-09 21:17:46 -0500

    Adding tweets of the user when a user is getting by id

--- typeormProject/src/controllers/user.ts
+++ typeormProject/src/controllers/user.ts
@@ -1,36 +1,47 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import { User } from '../entity/User';
+import { Tweet } from '../entity/tweets';
 import { validate } from 'class-validator';
 
-export const getUsers = async (req: Request, res: Response): Promise<Response> =>{
+export const getUsers = async (req: Request, res: Response): Promise<Response> => {
     try {
-        const users = await getRepository(User).find();
+        const users = await getRepository(User).find();        
         return res.status(200).json(users);
     }
     catch (error) {
         console.log('Error: ', error.message);
         return res.status(400).send();
-    };    
+    };
 };
 
-export const getUser = async (req: Request, res: Response): Promise<Response> =>{
+export const getUser = async (req: Request, res: Response): Promise<Response> => {
     try {
         const user = await getRepository(User).findOne(req.params.id);
-        return res.status(200).json(user);
+        const tweets = await getRepository(Tweet).find();        
+        var result: Array<any>=[];
+        result.push(user);
+        if (tweets) {
+            for (let i = 0; i < tweets.length; i++) {
+                if (tweets[i].user === req.params.id) {
+                    result.push(tweets[i])
+                }
+            }            
+        }        
+        return res.status(200).json(result);
     }
     catch (error) {
         console.log('Error: ', error.message);
         return res.status(400).send();
-    };    
+    };
 };
 
-export const postUser = async (req: Request, res: Response): Promise<Response> =>{
-    try{
+export const postUser = async (req: Request, res: Response): Promise<Response> => {
+    try {
         const user = getRepository(User).create(req.body);
         const validations = await validate(user);
 
-        if(validations.length === 0) {
+        if (validations.length === 0) {
             const response = await getRepository(User).save(user);
             return res.status(201).json(response);
         }
@@ -39,37 +50,37 @@ export const postUser = async (req: Request, res: Response): Promise<Response> =
     catch (error) {
         console.log('Error: ', error.message);
         return res.status(400).send();
-    };    
+    };
 };
 
-export const updateUser = async (req: Request, res: Response): Promise<Response> =>{
+export const updateUser = async (req: Request, res: Response): Promise<Response> => {
     try {
         const user = await getRepository(User).findOne(req.params.id);
-    if (user) {
-        getRepository(User).merge(user, req.body);
-        const validations = await validate(user);
+        if (user) {
+            getRepository(User).merge(user, req.body);
+            const validations = await validate(user);
 
-        if (validations.length === 0) {
-            const response = await getRepository(User).save(user);
-            return res.status(200).json(response);    
+            if (validations.length === 0) {
+                const response = await getRepository(User).save(user);
+                return res.status(200).json(response);
+            }
+            res.status(400).json(validations);
         }
-        res.status(400).json(validations);        
-    }
-    return res.status(404).json({message: "User does not exit."});    
+        return res.status(404).json({ message: "User does not exit." });
     }
     catch (error) {
         console.log('Error: ', error.message);
         return res.status(400).send();
-    };    
+    };
 };
 
-export const deleteUser = async (req: Request, res: Response): Promise<Response> =>{
+export const deleteUser = async (req: Request, res: Response): Promise<Response> => {
     try {
         const result = getRepository(User).delete(req.params.id);
-        return res.status(200).json({message: "User deleted."});
+        return res.status(200).json({ message: "User deleted." });
     }
     catch (error) {
         console.log('Error: ', error.message);
         return res.status(400).send();
-    };    
+    };
 };
